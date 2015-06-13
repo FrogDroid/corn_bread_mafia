1e479d64e4602f9580bbe248236c437714fc24fd
Jeremiah <jeremiah.sommerfeld@cbtravel.com>
2015-06-13 15:28:20 -0600

    added breadpan.c for our shells main c file.

---
+++ breadpan.c
@@ -0,0 +1,11 @@
+//import necessary dependancies
+
+//When the shell is started print breadpan$> to the screen as a shell prompt.
+
+//Run a while loop that will wait for user input
+
+//if the input == 'exit' call exit(0);
+
+//if the input is an accepted command run the call for that command with the associated parameters
+
+//if the input is not recognized print an error message saying it is an unrecognized command
