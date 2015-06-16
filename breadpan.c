c1e65c4cec244a5cdb70ba676ab733b9cfc1cf67
brianbrandt50@msn.com <brianbrandt50@msn.com>
2015-06-15 23:48:35 -0700

    I put all parse functionality into the function parse() it returns an array of strings, I have cd working for root, but still cant get it to take arguments, I also defined a grepPat for strtok, and a buffer i was playing with based off a tutorial I saw. See your gmail chat. We're close, I can feel it! I work until 5 Pacific time, so I will be on around 630 mountain

--- breadpan.c
+++ breadpan.c
@@ -7,12 +7,15 @@
 #include <sys/wait.h>// import wait for prototyping
 #include <limits.h>
 //basic error handling from assignment sheet
+#define grepPat " \t\r\n\a" //defined a grep pattern
+#define buffer 64 //define buffer size
 void printError(){
 	char error_message[30] = "An error has occurred\n";
 	write(STDERR_FILENO, error_message, strlen(error_message));
 }
 
 void breadLs(char *arguments[]);
+char **parse(char *in);
 
 int main(int argc, char *argv[])
 {
@@ -59,29 +62,18 @@ int main(int argc, char *argv[])
     		}
 		
 		//no we have our input as a string, time to start parsing it out and switching between commands.
-		char * command = strtok(input, " ");
-//		printf("\n command entered: %s \n", command);
-
-		char *arguments[10];
-                int a = 0;
-		arguments[a] = strtok( NULL , " " );
-                while(arguments[a] != NULL)
-                {
-                        a++;
-			if(a > 10)
-				break;
-                        arguments[a] = strtok( NULL , " " );
-                }
-
+		char * command = strtok(input, grepPat);
+		//printf("\n command entered: %s \n", command);
 		//we can't use a switch because C is retarded.
 		//so here's our if else if bucket code
+		char **args= parse(input);//create out array of strings to inpuit args...hoping this will work
 		if(strcmp (command, "exit") == 0)
 		{
 			exit(0);
 		}
 		else if( strcmp ( command, "ls" ) == 0 )
 		{
-			breadLs(arguments);	
+			breadLs(args);	
 		}
 		//todo: enter in the rest of the these commands
 		
@@ -97,7 +89,16 @@ int main(int argc, char *argv[])
 			printf("%s\n",workDirectory);
                 }
 
-		
+		 else if( strcmp ( command, "cd" ) == 0 ){
+               	       
+				if(args[1]==NULL){
+                                chdir(getenv("HOME"));
+                        }
+                        else{
+                        	chdir(args[1]); 
+                        }
+                }
+	
 		else
 		{
 			printError(); //not a valid command.
@@ -106,7 +107,7 @@ int main(int argc, char *argv[])
 	exit(0); //safe exit at end of while loop.
 }
 
-void breadLs(char *arguments[]){
+void breadLs(char **arguments){ //changed this to accept array of strings
 	 int i = 1;
 	 int j = 1;
          int child = fork();
@@ -132,5 +133,36 @@ void breadLs(char *arguments[]){
          else {
          	perror("fork");//error
          }
+
+}
+
+
+char **parse(char *in){ //function to parse the input into an array of strings
+	int argbuffer= buffer; //buffer
+	char *argument; //string
+        char **arguments=malloc(argbuffer * sizeof(char*)); //array of string allocated via malloc
+        int a = 0; //counter
+	if (!arguments) { //check for array of strings setup
+       		printf("allocation error\n");
+                exit(EXIT_FAILURE);
+       	}
+
+      	argument= strtok(in, grepPat);//read string into array
+      	while(argument!= NULL) //while not null read
+      	{	
+
+        	arguments[a] = argument; //read into array of strings
+        	a++;
+		if (a >=argbuffer) { //make buffer larger if needed
+     			 argbuffer += buffer;
+     			 arguments = realloc(arguments, argbuffer * sizeof(char*));
+     			 if (!arguments) {
+       			 exit(EXIT_FAILURE);
+     			 }
+  		 }
+        	argument = strtok(NULL, grepPat);//next token
+     	 }
+     	arguments[a]=NULL;//set last to null
+	return arguments; //return array of strings	
 
 }
